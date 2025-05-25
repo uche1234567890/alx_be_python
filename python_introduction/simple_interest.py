2733c1596de6e77a97dff01c31beca6d6f264a37
uche Nwankwo <ucheo.nwankwo@gmail.com>
2025-05-25 21:15:57 +0100

    Task1

---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,7 @@
+# Assign specific values to variables
+principal = 1000
+rate = 0.05
+time = 3
+# Calculate simple interest
+interest = principal * rate * time
+print(f"the simple interest is {interest}")
