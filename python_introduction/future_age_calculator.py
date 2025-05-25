32e7dc69cee0fcaf7626cae07aa376f9eb5dc062
uche Nwankwo <ucheo.nwankwo@gmail.com>
2025-05-25 22:40:41 +0100

    Task4

---
+++ python_introduction/future_age_calculator.py
@@ -0,0 +1,9 @@
+# Prompt the user for their current age
+current_age = int(input("How old are you? "))
+
+# Calculate future age in the year 2050
+years_to_add = 2050 - 2023  # 27 years
+future_age = current_age + years_to_add
+
+# Print the result
+print(f"In 2050, you will be {future_age} years old.")
