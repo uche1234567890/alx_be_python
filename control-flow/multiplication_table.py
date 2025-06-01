971af92e79f5ef79b0529cf632b746591723f0aa
uche Nwankwo <ucheo.nwankwo@gmail.com>
2025-06-01 23:07:31 +0100

    Task1

---
+++ control-flow/multiplication_table.py
@@ -0,0 +1,6 @@
+number = int(input("Enter a number to see its multiplication table: "))
+
+# Generate and Print the Multiplication Table
+for i in range(1, 11):
+    product = number * i
+    print(f"{number} * {i} = {product}")
