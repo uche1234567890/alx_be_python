1ba3b29714d9e21adb261176339b75259cdeabed
uche Nwankwo <ucheo.nwankwo@gmail.com>
2025-06-01 04:03:48 +0100

    Task0

--- control-flow/weather_advice.py
+++ control-flow/weather_advice.py
@@ -1,7 +1,7 @@
 weather = input("What's the weather like today? (sunny/rainy/cold): ").strip().lower()
 
 if weather == "sunny":
-    print(" Wear a t-shirt and sunglasses.")
+    print("Wear a t-shirt and sunglasses")
 elif weather == "rainy":
     print("Don't forget your umbrella and a raincoat")
 elif weather == "cold":
