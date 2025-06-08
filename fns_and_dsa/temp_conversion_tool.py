10e1aad25e44e24b1a5fb275551ccf8b0e644893
uche Nwankwo <ucheo.nwankwo@gmail.com>
2025-06-08 23:16:14 +0100

    changes

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -1,12 +1,13 @@
-FAHRENHEIT_TO_CELSIUS_FACTOR = 5 / 9
 CELSIUS_TO_FAHRENHEIT_FACTOR = 9 / 5
+FAHRENHEIT_TO_CELSIUS_FACTOR = 5 / 9
+FAHRENHEIT_OFFSET = 32
 
 def convert_to_celsius(fahrenheit):
     """Convert Fahrenheit to Celsius."""
-    return (fahrenheit - 32) * FAHRENHEIT_TO_CELSIUS_FACTOR
+    return (fahrenheit - FAHRENHEIT_OFFSET) * FAHRENHEIT_TO_CELSIUS_FACTOR
 def convert_to_fahrenheit(celsius):
     """Convert Celsius to Fahrenheit."""
-    return (celsius * CELSIUS_TO_FAHRENHEIT_FACTOR) + 32
+    return (celsius * CELSIUS_TO_FAHRENHEIT_FACTOR) + FAHRENHEIT_OFFSET
 
 temperature = float(input("Enter a temperature: "))
 unit = input("Is this temperature in Fahrenheit or Celsius? (F/C): ").strip().upper()
