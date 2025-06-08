1f66aca75df259054d5e2c8b236d8d7520e0b922
uche Nwankwo <ucheo.nwankwo@gmail.com>
2025-06-08 20:56:10 +0100

    Task0

---
+++ fns_and_dsa/arithmetic_operations.py
@@ -0,0 +1,13 @@
+def perform_operation(num1: float, num2: float, operation: str) -> float | str:
+    if operation == 'add':
+        return num1 + num2
+    elif operation == 'subtract':
+        return num1 - num2
+    elif operation == 'multiply':
+        return num1 * num2
+    elif operation == 'divide':
+        if num2 == 0:
+            return "DIVIDE_BY_ZERO"
+        return num1 / num2
+    else:
+        return "INVALID_OPERATION"
