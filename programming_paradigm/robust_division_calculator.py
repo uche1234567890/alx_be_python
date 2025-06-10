f2a7f1c09442942fdc2d81d75b32274f38ce0e5a
uche Nwankwo <ucheo.nwankwo@gmail.com>
2025-06-10 05:18:07 +0100

    changes

--- programming_paradigm/robust_division_calculator.py
+++ programming_paradigm/robust_division_calculator.py
@@ -10,4 +10,4 @@ def safe_divide(numerator, denominator):
             return "Error: Cannot divide by zero."
 
     except ValueError:
-        return "Error: Please enter numeric values."
+        return "Error: Please enter numeric values only."
