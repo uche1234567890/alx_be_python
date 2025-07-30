b91051add3111a966c04a7019ea21b241b070d0b
uche Nwankwo <ucheo.nwankwo@gmail.com>
2025-07-30 20:32:03 +0100

    Task 2

---
+++ oop/polymorphism_demo.py
@@ -0,0 +1,18 @@
+class shape:
+    def area(self):
+        raise NotImplementedError("Subclasses must implement this method")
+    
+class rectangle(shape):
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+
+    def area(self):
+        return self.width * self.height
+
+class circle(shape):
+    def __init__(self, radius):
+        self.radius = radius
+
+    def area(self):
+        return 3.14 * self.radius * self.radius
